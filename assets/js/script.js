433cbddf478c7ee5a40d99d450cc85d99adfc135
chachachacharlirae <61812759+chachachacharlirae@users.noreply.github.com>
2020-04-11 20:30:05 -0500

    next section updates refractor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,34 @@
+var formEl = document.querySelector("#task-form");
 
-
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var buttonEl = document.querySelector("#save-task");
 
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  }
+var taskFormHandler = function (event) {
+  event.preventDefault();
 
-buttonEl.addEventListener("click", createTaskHandler);
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  };
+  createTaskEl(taskDataObj);
+
+};
+
+var createTaskEl = function(taskDataObj) {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" +
+  taskDataObj.type  + "</span>";
+
+
+  listItemEl.appendChild(taskInfoEl);
+  tasksToDoEl.appendChild(listItemEl);
+
+}
+
+formEl.addEventListener("submit", taskFormHandler);
